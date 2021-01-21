87f0b3ded467c060ac06063e89fa7d5d5ab932fe
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-20 23:42:55 -0600

    A estrella

---
+++ Aestrella/AStarTest.java
@@ -0,0 +1,72 @@
+package IA.Aestrella;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AStarTest {
+
+    public static void main(String[] args) {
+        Node initialNode = new Node(2, 1);
+        Node finalNode = new Node(2, 5);
+        int rows = 6;
+        int cols = 7;
+        AStar aStar = new AStar(rows, cols, initialNode, finalNode);
+        ArrayList<Node> blocksArray = new ArrayList<>();
+        blocksArray.add(new Node(1,3));
+        blocksArray.add(new Node(2,3));
+        blocksArray.add(new Node(3,3));
+        blocksArray.add(new Node(4,2));
+        aStar.setBlocks(blocksArray);
+
+        List<Node> path = aStar.findPath();
+
+        for (Node node : path) {
+            System.out.println(node);
+        }
+
+        //Search Area
+        //      0   1   2   3   4   5   6
+        // 0    -   -   -   -   -   B   B
+        // 1    -   #   -   #   -   #   -
+        // 2    -   I   -   B   -   F   L
+        // 3    -   #   -   #   -   #   E
+        // 4    B   -   -   -   L   E   -
+        // 5    -   #   -   #   -   #   -
+
+        //Expected output with diagonals
+        //Node [row=2, col=1]
+        //Node [row=1, col=2]
+        //Node [row=0, col=3]
+        //Node [row=1, col=4]
+        //Node [row=2, col=5]
+
+        //Search Path with diagonals
+        //      0   1   2   3   4   5   6
+        // 0    -   -   -   *   -   -   -
+        // 1    -   -   *   B   *   -   -
+        // 2    -   I*  -   B   -  *F   -
+        // 3    -   -   -   B   -   -   -
+        // 4    -   -   -   -   -   -   -
+        // 5    -   -   -   -   -   -   -
+
+        //Expected output without diagonals
+        //Node [row=2, col=1]
+        //Node [row=2, col=2]
+        //Node [row=1, col=2]
+        //Node [row=0, col=2]
+        //Node [row=0, col=3]
+        //Node [row=0, col=4]
+        //Node [row=1, col=4]
+        //Node [row=2, col=4]
+        //Node [row=2, col=5]
+
+        //Search Path without diagonals
+        //      0   1   2   3   4   5   6
+        // 0    -   -   *   *   *   -   -
+        // 1    -   -   *   B   *   -   -
+        // 2    -   I*  *   B   *  *B   -
+        // 3    -   -   -   B   -   -   -
+        // 4    -   -   -   -   -   -   -
+        // 5    -   -   -   -   -   -   -
+    }
+}
