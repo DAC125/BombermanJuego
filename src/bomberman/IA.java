7f0c57ee5b67f578c018d701e993887dfca5f362
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 02:22:09 -0600

    comentarios

--- src/bomberman/IA.java
+++ src/bomberman/IA.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+//Esta clase se encarga de, según el nivel de inteligencia de cierto enemigo, devolver el
+//siguiente movimiento que este debe de realizar dentro del mapa
 public class IA {
     private Nivel mapa;
     ArrayList<int[]> movimientosRand = new ArrayList<>();
 
+    //Constructor de la IA
     public IA(Nivel mapa){
         this.mapa = mapa;
         this.movimientosRand = genMov();
     }
 
-    public ArrayList<int[]> genMov(){ //genera las posibilidades de movimientos randoms
+    //genera las posibilidades de movimientos randoms
+    public ArrayList<int[]> genMov(){
         ArrayList<int[]> res = new ArrayList<>();
         int[] posibles = {0,1};
         for (int i = 0; i<2 ; i++){
@@ -24,32 +28,32 @@ public class IA {
                     if(i == 1){
                         a[0]=posibles[i];
                         a[1]=posibles[j];
-                        //System.out.println(a[0]+", "+a[1]);
                         res.add(a);
                         b[0]=-1;
                         b[1]=posibles[j];
-                        //System.out.println(b[0]+", "+b[1]);
                         res.add(b);
                     }else{
                         a[0]=posibles[i];
                         a[1]=posibles[j];
-                        //System.out.println(a[0]+", "+a[1]);
                         res.add(a);
                         b[1]=-1;
                         b[0]=posibles[i];
-                        //System.out.println(b[0]+", "+b[1]);
                         res.add(b);
                     }
                 }
             }
         }return res;
     }
+
+    //Genera un número random entre cierto rango
     public int getRandomNumber(int min, int max) { //genera un numero random desde ciero rango, tierniendo el numero minimo inclusivo, pero el mayor numero exclusivo
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    public int[] GenRandom(int x, int y){ //genera un numero random y sortea uno de los movimientros randoms que generaba la anterior funcion y evalua si es valida
-        int flag = 0;//avisa cuando ya encontro una posicion random valida
+    //Genera un número random y sortea uno de los movimientros randoms que generaba la anterior funcion
+    //y evalua si es valida avisa cuando ya encontro una posicion random valida
+    public int[] GenRandom(int x, int y){
+        int flag = 0;
         int[] res = new int[2];
         while(flag != 1){
             int rand = getRandomNumber(0,4);
@@ -66,6 +70,9 @@ public class IA {
         return res;
     }
 
+    /*Todos los niveles reciben los mismo: "x" y "y" de Bomberman y del Enemigo */
+
+    //Solo persigue a BomberMan si está en el mismo x,y y sin muros en el camino
     public int[] nivel1(int bx, int by, int posx ,int posy){//bx y by son las posiciones de bomberman y posx y posy son las posiciones del enemigo
         int[] res = new int[2];
         if(bx == posx && bx%2 != 0){//si la posicion del enemigo esta en la misma fila que bombermam y el numero es par
@@ -121,13 +128,15 @@ public class IA {
             return GenRandom(posx,posy);
         }
     }
+
+    //se mueve aleatoriamente el 75 % de las veces y otras persigue a nuestro héroe
     public int[] nivel2(int bx, int by, int posx ,int posy){
         double rand = Math.random();
         if(rand<=0.75){
             int res[] = new int[2];
             Node objetivo = new Node(bx,by);
             Node inicio = new Node(posx,posy);
-            AStar aStar = new AStar(this.mapa.getLargo(), this.mapa.getAncho(), inicio, objetivo);
+            AStar aStar = new AStar(this.mapa.getLargo(), this.mapa.getAncho(), inicio, objetivo);//Busca el siguiente mov mas corto hasta bomberman
             aStar.setBlocks(this.mapa.getObstaculos());
             List<Node> camino = aStar.findPath();
             try {
@@ -144,6 +153,8 @@ public class IA {
         }
     }
 
+    //Si Bomberman está a menos de 30 cuadros de distancia del enemigo, el enemigo lo persigué, si no
+    //se mueve aleatorio
     public int[] nivel3(int bx, int by, int posx ,int posy){
         int res[] = new int[2];
         Node objetivo = new Node(bx,by);
@@ -151,8 +162,7 @@ public class IA {
         AStar aStar = new AStar(this.mapa.getLargo(), this.mapa.getAncho(), inicio, objetivo);
         aStar.setBlocks(this.mapa.getObstaculos());
         List<Node> camino = aStar.findPath();
-        int total = camino.size();
-        //System.out.println("Total camino = "+ total);
+        int total = camino.size();//obtiene el largo de la ruta más corta hasta Bomber
         if(total<=30){
             try {
                 Node respuesta = camino.get(1);
@@ -167,6 +177,8 @@ public class IA {
             return GenRandom(posx,posy);
         }
     }
+
+    //Siempre persigue a BomberMan, mediante el algoritmo A*
     public int[] nivel4(int bx, int by, int posx ,int posy){
         int res[] = new int[2];
         Node objetivo = new Node(bx,by);
@@ -184,8 +196,8 @@ public class IA {
         return res;
     }
 
-
-    public void animarTemp(int bx, int by, int pEx, int pEy){//funcion temporal que grafica el mapa del juego y sirve para probar las IAs, tiene un delay de 1s por cada iteracion
+    //funcion temporal que grafica el mapa del juego y sirve para probar las IAs, tiene un delay de 1s por cada iteracion
+    public void animarTemp(int bx, int by, int pEx, int pEy){
         this.mapa.mapa[bx][by] = 'B';
         boolean igual = false;
         while(igual == false){
