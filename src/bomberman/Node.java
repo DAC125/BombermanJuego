1b7544420c5b3f18715d7357ba65dd17ecac94eb
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 01:20:07 -0600

    comentarios

--- src/bomberman/Node.java
+++ src/bomberman/Node.java
@@ -1,25 +1,34 @@
 package bomberman;
 
+//clase encargada de almacenar la información necesaria para el algoritmo A* principalmente
+//Aunque se usa en otros metodos
+//representa un lugar en la matriz por la que se mueve BomberMan
 public class Node {
 
     private int g;//costo actual, los pasos que hemos tenido que dart
     private int h;//heuristica;;x e y de la casilla actual y destino; cual es la diferencia= valor absoluto y lo sumamos
     private int f;;//g+h costo de llegar a hasta acá y cuanto crremos que nos
-    private int row;
-    private int col;
-    private boolean isBlock;
-    private Node parent;
+    private int row;//fila del Nodo
+    private int col;//columna del Nodo
+    private boolean isBlock;//Indica si el nodo actual, es un bloque, para saber si por acá, el algoritmo no puede pasar
+    private Node parent;//Usado en A*, A través de los padres de conoce la ruta más corta del grafo(Matriz)
 
+    //Constructor del Nodo; recibe el x y el en el que se encuentra el nodo
     public Node(int row, int col) {
         super();
         this.row = row;
         this.col = col;
     }
 
+    //Recibe el nodo al que se quiere llegar, y mediante una heurística
+    //calcula el costo aproximado de llegar hasta ella desde el Nodo Actual
+    // h = |filaDestino-filaActual|+|colDestino-colActual|
     public void calculateHeuristic(Node finalNode) {
         this.h = Math.abs(finalNode.getRow() - getRow()) + Math.abs(finalNode.getCol() - getCol());
     }
 
+    //Calcula el  costo que se ha recorrido, desde el nodo inicial de la ruta que se busca, hasta
+    //el nodo actual
     public void setNodeData(Node currentNode, int cost) {
         int gCost = currentNode.getG() + cost;
         setParent(currentNode);
@@ -27,6 +36,7 @@ public class Node {
         calculateFinalCost();
     }
 
+    //El costo de ahora, es el mejor del que se tiene registro? Si es así, sigue por el nodo actual buscando
     public boolean checkBetterPath(Node currentNode, int cost) {
         int gCost = currentNode.getG() + cost;
         if (gCost < getG()) {
@@ -36,11 +46,14 @@ public class Node {
         return false;
     }
 
+    //Suma el costo heurístico mas el costo actual
+    //por lo que es el costo aprox total
     private void calculateFinalCost() {
         int finalCost = getG() + getH();
         setF(finalCost);
     }
 
+    //Sí, si dos objetos son iguales
     @Override
     public boolean equals(Object arg0) {
         Node other = (Node) arg0;
@@ -51,6 +64,8 @@ public class Node {
     public String toString() {
         return "Node [row=" + row + ", col=" + col + "]";
     }
+
+    /*Setters and getters*/
 
     public int getH() {
         return h;
