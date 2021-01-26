2ba34cd7fb0b5655d879727c19a13c660ae3bbba
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 02:01:23 -0600

    comentarios

--- src/bomberman/Teclado.java
+++ src/bomberman/Teclado.java
@@ -3,10 +3,14 @@ package bomberman;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+//clase encargada de actualizar las entradas del usuario por medio del teclado
 public class Teclado implements KeyListener {
-    private boolean[] teclas = new boolean[120];
+
+    private boolean[] teclas = new boolean[120];//Códigos de las flechas
     public boolean arriba, abajo, izquierda, derecha, espacio;
 
+    //cuando se presiona una tecla, setea su código en "teclas" en true, lo que indica que esa tecla está
+    //siendo pulsada
     public void actualiza(){
         arriba = teclas[KeyEvent.VK_UP] || teclas[KeyEvent.VK_W];
         abajo = teclas[KeyEvent.VK_DOWN] || teclas[KeyEvent.VK_S];
