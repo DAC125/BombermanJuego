55840a80a39e3b1bf0956a3d96b703543becdd7b
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 11:13:35 -0600

    comentarios

--- src/bomberman/Tablero.java
+++ src/bomberman/Tablero.java
@@ -3,8 +3,9 @@ package bomberman;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+//Clase encargada de manejar todo lo relacionado al tablero, que es la representación de lo que está en pantalla
 public class Tablero {
-    protected Nivel nivel;
+    protected Nivel nivel;//Nivel actual del Juego
     protected Juego juego;
     protected Teclado teclado;
     protected Pantalla pantalla;
@@ -12,21 +13,24 @@ public class Tablero {
     private int puntos = JuegoConfig.PUNTOS;
     private int vidas = JuegoConfig.VIDAS;
     private int pantallaMostar = -1;  //1: fin de juego, 2: cambiar nivel, 3: juego en pausa
-    public int xPlayer = 48;
-    public int yPlayer = 48;
+    public int xPlayer = 48;//Pos inicial del Juego
+    public int yPlayer = 48;//Pos inicial del Juego
     private int movimiento = 0, estado = 1;
 
     private Animacion animAbajo,animArr,animIzq,animDer;
 
+    //Constructor de la clase
     public Tablero(Juego juego, Teclado teclado, Pantalla pantalla){
+        //Inicializa los componentes básicos
         this.juego = juego;
         this.teclado = teclado;
         this.pantalla = pantalla;
 
+        //E inicia el juego en el nivel 1
         cambiarNivel(1);
-
     }
 
+    //Actualiza como se ve el personaje
     public void actualiza(){
         if(juego.estaPausado())return;
         animAbajo.tick();
@@ -38,12 +42,16 @@ public class Tablero {
         //nivel.imprimir(nivel.getMapa());
 
     }
+
+    //encargado de animar el personaje
     public void animacion(){
         animAbajo = new Animacion(250,Imagenes.player_abajo);
         animArr = new Animacion(250,Imagenes.player_arriba);
         animIzq = new Animacion(250,Imagenes.player_izquierda);
         animDer = new Animacion(250,Imagenes.player_derecha);
     }
+
+    //Renderiza al jugador o BomberMan en pantalla según el moviemiento que tenga
     public BufferedImage renderPlayer(){
 
         if (movimiento == 0){
@@ -69,21 +77,25 @@ public class Tablero {
         return null;
     }
 
+    //renderiza a los villanos
     public Villano renderVillano(int index){
         Villano villano = nivel.getEnemigoEspecifico(index);
         return villano;
 
     }
 
+    //detecta posiciones en el tablero de "char" para saber si las entidades se pueden seguir moviendo en x dirección
     public boolean colicion(int x, int y){
         char[][] mapa = nivel.getMapa();
-        if (mapa[y][x]==' '||mapa[y][x]=='B'){
+        if (mapa[y][x]==' '||mapa[y][x]=='B'){//Si choca con algo, quiere decir que no puede pasar
             return false;
         }else{
             return true;
         }
     }
 
+    //Método encargado de actualizar el movimiento de BomberMan captando la entrada por teclado
+    //del Jugador, se encarga de ver si hay colisiones y moverlo
     private void actualizaPlayer(){
 
         movimiento = 0;
@@ -141,6 +153,7 @@ public class Tablero {
 
     }
 
+    //se encarga de renderizar todo lo relacionado al tablero
     public BufferedImage[][] renderTablero() {
         if( juego.estaPausado()) return null;
 
@@ -173,8 +186,7 @@ public class Tablero {
 
     }
 
-
-
+    //Método encargado de cambiar y generar el nivel que se muestra en pantalla
     public void cambiarNivel(int lvl){
         tiempo = JuegoConfig.TIEMPO;
         pantallaMostar = 2;
@@ -183,6 +195,7 @@ public class Tablero {
 
     }
 
+    //Dibuja la pantalla para ser capaz de verla en "pantalla"
     public void drawScreen(Graphics g) {
         switch (pantallaMostar) {
             /*case 1:
@@ -215,6 +228,7 @@ public class Tablero {
         return vidas;
     }
 
+    //resta al tiempo restante del Juego
     public int restaTiempo() {
         if(juego.estaPausado())
             return this.tiempo;
