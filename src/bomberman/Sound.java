53718f277a303c8502598645d7f5bfd3931d9108
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 00:30:44 -0600

    comentarios

--- src/bomberman/Sound.java
+++ src/bomberman/Sound.java
@@ -4,23 +4,25 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
+//Clase encargada de Reproducir los sonidos necesarios para el juego
 public class Sound {
 
-
-
+    //Metodo que recibe una dirección de algún sonido, y si este se reproduce una y otra vez
     private void reproducirSonido(String nombreSonido, int loop){
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(nombreSonido).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-            clip.start();
-            if(loop==1){
+            clip.start();//reproduce el clip creado
+            if(loop==1){//Reproduce indefinidamente
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
             }
         } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
             System.out.println("Error al reproducir el sonido.");
         }
     }
+
+    /*Metodos que llaman a la función anterior con un archivo .wav para su reproducción*/
 
     public void reproducirSonidoMuerte(){
         reproducirSonido("res/sonidos/Muerte1.wav",0);
