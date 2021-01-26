b0469e0e675f239dbac072fab3cbaac096ea64ec
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 10:58:10 -0600

    comentarios

--- src/bomberman/Nivel.java
+++ src/bomberman/Nivel.java
@@ -2,7 +2,8 @@ package bomberman;
 
 import java.util.ArrayList;
 
-
+//Esta clase se encarga de cargar cada nivel del mapa
+//crea el tablero de juego, coloca enemigos y elije que enemigos poner, así como calcula la probabilidades de cada clase de enemigo
 public class Nivel {
     public char mapa[][];
     private int largo;
@@ -14,6 +15,7 @@ public class Nivel {
     private int[][] probabilidadesEnemigos;
     private ArrayList<Villano> enemigos = new ArrayList<>();
 
+    //Constructor vacio de la clase
     public Nivel(){
 
     }
@@ -24,23 +26,29 @@ public class Nivel {
         return this.ancho;
     }
 
+
     public ArrayList<Node> getObstaculos(){
         return this.obstaculos;
     }
 
+    //recibe el largo y el ancho del mapa, la probabiliadad  de tener un ladrillo en una pos
+    // y el numero de villanos, todos estos datos están datos por el archivo de configuración del Juego
     public Nivel(int largo, int ancho, float probabilidadMuro, int nivel, int numVillanos){
         this.ancho = ancho;
         this.largo = largo;
         this.proba = probabilidadMuro/100;
         this.nivel = nivel;
         this.numVill = numVillanos;
-        this.obstaculos = new ArrayList<>();
-        this.probabilidadesEnemigos = calcularProbabilidades();
-        this.mapa = crearTablero(ancho,largo);
-        this.enemigos = colocarEnemigos();
+        this.obstaculos = new ArrayList<>();//Los muros que hay en el mapa
+        this.probabilidadesEnemigos = calcularProbabilidades();//
+        this.mapa = crearTablero(ancho,largo);//
+        this.enemigos = colocarEnemigos();//
         imprimir(this.mapa);
     }
 
+    //se crea un tablero de Juego, con todos sus muros,
+    //Cuando una fila impar se tope con una col impar, se coloca un muro de acero(no se destruye ni atraviesa)"X"
+    //El resto del mapa se llena de muros según la probabilidad de estos de aparecer en el archivo de conf"_"
     public char[][] crearTablero(int a, int l){
         char board[][] = new char[l][a];
         for(int i = 0; i < l; i++){
@@ -48,7 +56,7 @@ public class Nivel {
                 if(j == 0 || j == a-1 || i == 0 || i == l-1 ) {
                     Node nuevo = new Node(i,j);
                     board[i][j] = 'X';
-                    this.obstaculos.add(nuevo);
+                    this.obstaculos.add(nuevo);//se rellena una lista con los muros que se van colocando
                 }else if(j%2 == 0 && i%2 == 0){
                     Node nuevo = new Node(i,j);
                     board[i][j] = 'X';
@@ -58,7 +66,7 @@ public class Nivel {
                         board[i][j] = ' ';
                     }else{
                         double rand = Math.random();
-                        if(rand<=this.proba){
+                        if(rand<=this.proba){//prob según archivo de configuración
                             board[i][j] = '_';
                             Node nuevo = new Node(i,j);
                             this.obstaculos.add(nuevo);
@@ -69,18 +77,20 @@ public class Nivel {
                 }
             }
         }
-        board[1][1]='B';
+        board[1][1]='B';//Se coloca el bomberman
         return board;
     }
+
+    //Método que se encarga de rellenar el mapa con el Número de enemigos que se indica en el archivo de conf
     private ArrayList<Villano> colocarEnemigos(){
-        ArrayList<Villano> lista = new ArrayList<>();
-        int temp = this.numVill;
+        ArrayList<Villano> lista = new ArrayList<>();//Una lista con objetos del tipo villano que se van a tener en el mapa
+        int temp = this.numVill;//Número de villanos a colocar
         while(temp!=0){
-            int xRand = getRandomNumber(0,this.largo);
+            int xRand = getRandomNumber(0,this.largo);//pos a colocar el enemigo en rand
             int yRand = getRandomNumber(0,this.ancho);
             if( this.mapa[xRand][yRand] == ' ' && xRand>3 && yRand>3){
                 int rand = (int)(Math.random()*100);
-                int nivel = seleccionarEnemigo(this.probabilidadesEnemigos,rand);
+                int nivel = seleccionarEnemigo(this.probabilidadesEnemigos,rand);//Elije el tipo de enemigo a colocar
                 Villano nuevo = new Villano(nivel,xRand,yRand, this);
                 char enemigo  = (char)(96 +nuevo.getNivel());
                 lista.add(nuevo);
@@ -90,10 +100,12 @@ public class Nivel {
         }return lista;
     }
 
+    //Número random en un rango(min, max)
     public int getRandomNumber(int min, int max) { //genera un numero random desde ciero rango, tierniendo el numero minimo inclusivo, pero el mayor numero exclusivo
         return (int) ((Math.random() * (max - min)) + min);
     }
 
+    //Selecciona un tipo de enemigo
     public int seleccionarEnemigo(int[][] rango, int valorRandom){
         for(int i = 0; i< rango.length; i++){
             //System.out.println("Random = " + valorRandom + " Min = " + rango[i][0] + " Max = " + rango[i][1]);
@@ -104,11 +116,14 @@ public class Nivel {
         return 1;
     }
 
+    //Imprime un tablero
     public void imprimir(char[][] tab){
         for(int i = 0; i<tab.length; i++){
             System.out.println(tab[i]);
         }
     }
+
+    //Calcula las probabilidades de cada tipo de enemigo con Poisson
     public int[][] calcularProbabilidades(){
         double[] pruebas = new double[this.nivel-1];
         for(int i = 1;i<this.nivel;i++){
@@ -129,6 +144,7 @@ public class Nivel {
         return crearMatrizProba(pruebas);
     }
 
+    //Crea una matriz con las probabilidades acumuladas de los enemigos
     private int[][] crearMatrizProba(double[] pruebas){
         int[][] rangos = new int[pruebas.length+1][2];
         int acum = 0;
@@ -148,6 +164,10 @@ public class Nivel {
         //System.out.println("Max = " + 100);
         return rangos;
     }
+
+    //Gracias a la formula de poisson, se determina que tipo de enemigo se debe de escoger
+    //esto tomando en cuenta el nivel del mapa actual y la cantidad de tipo de enemigos que pueden
+    //aparecer en el nivel actual
     private double distribucionEnemigos(int level,int numVill){
         double e = Math.exp(1);
         double res = ((Math.pow(e,(-1 * level))*Math.pow(level,numVill))/(factorial(numVill)));
@@ -155,7 +175,7 @@ public class Nivel {
         return res;
     }
 
-
+    //Factorial de n número
     public long factorial(int n) { //eficiente para numeros hasta el 20 (suficiente)
         long fact = 1;
         for (int i = 2; i <= n; i++) {
@@ -163,6 +183,8 @@ public class Nivel {
         }
         return fact;
     }
+
+    //dado un X y un Y, si ese x,y es un obstaculo, se elimina
     public void eliminarMuro(int x, int y){ //PROBAAAAAR!!!!!!
         int buscar[] = new int[2];
         buscar[0] = x;
@@ -179,6 +201,8 @@ public class Nivel {
     public char[][] getMapa() {
         return mapa;
     }
+
+    //Se encarga de mover al personaje por el tablero mediante coordenadas
     public void setMapaMov(int x, int y,int xp,int yp){
         char aux = mapa[y][x];
         mapa[y][x]=' ';
@@ -193,6 +217,7 @@ public class Nivel {
         return enemigos;
     }
 
+    //Devuelve el enemigo en el  índice "index"
     public Villano getEnemigoEspecifico(int index){
         return enemigos.get(index);
     }
