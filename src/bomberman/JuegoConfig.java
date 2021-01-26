599887baf4ccc8678b27bc8b61ae65981ee64437
ricardomj0499 <55812218+ricardomj0499@users.noreply.github.com>
2021-01-26 00:02:09 -0600

    comentarios

--- src/bomberman/JuegoConfig.java
+++ src/bomberman/JuegoConfig.java
@@ -4,30 +4,32 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+//clase encargada de setear todas las variables "globales" que se ocupan durante el Juego
 public class JuegoConfig {
     public static final int TAMAÑO_BLOQUE = 16,
                             FRECOBOMB = 1,
                             REDBOMB = 1,
-                            TIEMPO = 200,
-                            PUNTOS = 0,
-                            VIDAS = 3,
+                            TIEMPO = 200,//variables utilizadas en panelInfo en Frame
+                            PUNTOS = 0,//variables utilizadas en panelInfo en Frame
+                            VIDAS = 3,//variables utilizadas en panelInfo en Frame
                             DELAYTEMPLVL = 3,
                             ESCALA = 3;
 
     public static int LARGOMAPA=0,ANCHOMAPA=0;
 
     public static int LARGO, ANCHO;
-    public static int numVillanos = 5;
+    public static int numVillanos = 5;//Número de villanos totales que van a aparecer en el mapa
     public static int anchoVentana = 13,largoVentana = 15;
-    public static int probMuro = 0;
-    public static int velocidadPlay=0;
+    public static int probMuro = 0;//probabilidad de que exista en muro en cierta pos
+    public static int velocidadPlay=0;//velocidad del jugador
 
+    //Metodo encargado de leer el archivo de configuración del Juego
     public static void Leertxt(){
         String[] texto = new String[7];
         int index = 0;
 
         try {
-            File file =  new File("./config");
+            File file =  new File("./config");//ruta del archivo
             System.out.println(file.getAbsolutePath());
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file.getAbsolutePath()));
             String bfRead;
@@ -36,6 +38,7 @@ public class JuegoConfig {
                 index++;
             }
             String[] dimentions = texto[0].split(" ");
+            //se setean las variables
             ANCHOMAPA = Integer.parseInt(dimentions[0]);
             LARGOMAPA = Integer.parseInt(dimentions[1]);
             probMuro = Integer.parseInt(texto[1]);
